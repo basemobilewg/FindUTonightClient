946cd0d79a3e8be12bdd71a2a350e9f2f5d58e69
wuxiaobi <hoshino937@yahoo.co.jp>
2014-11-16 18:30:05 +0900

    SocketService実装

---
+++ src/common/Const.java
@@ -0,0 +1,17 @@
+package common;
+
+public final class Const {
+	
+	private Const() {};
+	
+	//SOCKETí êMópORDER
+	public final class ORDER {
+		private ORDER() {};
+		public static final String KEY_SELECT_SED = "1001";
+		public static final String KEY_SELECT_REC = "9001";
+	}
+	
+	
+
+
+}
